6f2aeb4dd870ac150cb7160a33ab02ffaa541763
jcamier <jcamier@gmail.com>
2019-06-05 19:48:07 -0500

    add react form

--- frontend/src/components/leads/Leads.js
+++ frontend/src/components/leads/Leads.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { getLeads } from "../../actions/leads";
+import { getLeads, deleteLead } from "../../actions/leads";
 
 export class Leads extends Component {
     static propTypes= {
-        leads: PropTypes.array.isRequired
+        leads: PropTypes.array.isRequired,
+        getLeads: PropTypes.func.isRequired,
+        deleteLead: PropTypes.func.isRequired,
+
     };
     componentDidMount() {
         this.props.getLeads();
@@ -14,11 +17,32 @@ export class Leads extends Component {
     render() {
         return (
 
-            <div>
-            <h1>Leads List</h1>
-            </div>
+            <Fragment>
+                <h2>Leads</h2>
+                <table className="table table-striped">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Message</th>
+                    <th> </th>
+                </tr>
+                </thead>
+                <tbody>
+                { this.props.leads.map(lead => (
+                    <tr key={lead.id}>
+                        <td>{lead.name}</td>
+                        <td>{lead.email}</td>
+                        <td>{lead.message}</td>
+                        <td><button onClick={this.props.deleteLead.bind(this, lead.id)} className="btn btn-danger btn-sm">Delete</button></td>
+                    </tr>
+                ))}
+                </tbody>
+                </table>
+            </Fragment>
 
-        )
+        );
     }
 }
 
@@ -26,4 +50,4 @@ const mapStateToProps = state => ({
     leads: state.leads.leads
 });
 
-export default connect(mapStateToProps, { getLeads }) (Leads);
+export default connect(mapStateToProps, { getLeads, deleteLead }) (Leads);
